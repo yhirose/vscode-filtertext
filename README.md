84451927ff7bb22e5d67c7cb0bd5911df2aceabd
yhirose <yuji.hirose.bug@gmail.com>
2015-11-21 21:40:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Filter Text extension for Visual Studio Code
+
+This extension provides a functionality to filter selected text through an external shell command such as `sort` and `uniq` commands.
+
+## Usage
+
+* Select text on the active editor document.
+* Press `Ctrl+Shift+F` or press `F1` and run the command named `Filter Text`.
+* Type command and press enter such as `sort -r`.
+* It replaces the selected text with the filtered text.
+
+![Filter selected text](images/filtertext.gif)
+
+## License
+
+MIT license (© 2015 Yuji Hirose)
---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+import * as child_process from 'child_process';
+import * as which from 'which';
+import * as shell_quote from 'shell-quote';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	var disposable = vscode.commands.registerCommand('extension.filterText', () => {
+		vscode.window.showInputBox({
+			placeHolder: 'Plese enter command name and arguments.'
+		}).then((entry: string) => {
+			if (entry) {
+                var args = shell_quote.parse(entry);
+				if (!args.length) {
+					return;
+				}
+				var name = args.shift();
+				
+				which(name, (err, path) => {
+					if (err) {
+						vscode.window.showErrorMessage('Invalid command is entered.');
+						return;
+					}
+					var filteredText = '';
+					var editor = vscode.window.activeTextEditor;
+					var filter = child_process.spawn(path, args);
+					if (!editor.selection.isEmpty) {
+						filter.stdin.write(editor.document.getText(editor.selection));
+						filter.stdin.end();
+					}
+					filter.stdout.on('data', function (data) {
+						filteredText += data;
+					});
+					filter.stdout.on('end', function () {
+						editor.edit((editBuilder) => {
+							editBuilder.replace(editor.selection, filteredText);
+						});
+					});
+				});
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
